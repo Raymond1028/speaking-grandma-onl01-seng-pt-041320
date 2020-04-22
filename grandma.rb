5e6ed07aea31f0cb80699f89ba8692a71dd83d58
Raymond Sims <lancelot53.rs@gmail.com>
2020-04-22 03:06:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,4 +1,21 @@
-# Write a speak_to_grandma method.
+
+def speak_to_grandma(phrase)
+  if phrase == "I LOVE YOU GRANDMA"
+   return "I LOVE YOU TOO PUMPKIN!" 
+elsif phrase == phrase.upcase
+return ""
+
+
+
+
+
+
+
+return
+  
+  return "HUH? ! SPEAK UP, SONNY"
+  
+  # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
