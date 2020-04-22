bc97a01a668a55f938231ad06e1c46a614318ec7
Raymond Sims <lancelot53.rs@gmail.com>
2020-04-22 03:10:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,12 +1,13 @@
 
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
-   return "I LOVE YOU TOO PUMPKIN!" 
-elsif phrase == phrase.upcase
-return "HUH?! SPEAK UP, SONNY!."
-else
-  return
+    return "I LOVE YOU TOO PUMPKIN!" 
+  elsif phrase !== phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!."
+  else
+  return "NO, NOT SINCE 1938!."
   
+end  
 
 
 
@@ -14,10 +15,7 @@ else
 
 
 
-return
-  
-  return "HUH? ! SPEAK UP, SONNY"
-  
+
   # Write a speak_to_grandma method.
 
 # Whatever you say to grandma, she should respond with
