ff5ba58ca84061eff232e41be82cf78501b510c3
Raymond Sims <lancelot53.rs@gmail.com>
2020-04-22 03:21:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,10 +8,6 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!."
   end
 end  
-speak_to_grandma("I LOVE YOU GRANDMA")
-speak_to_grandma("I love you Granma")
-speak_to_grandma("HELLO GRANDMA")
-
 
 
 
