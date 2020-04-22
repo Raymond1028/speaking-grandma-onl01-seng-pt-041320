1ee61be7c3b37ffcb7e98b2707dd19f898681879
Raymond Sims <lancelot53.rs@gmail.com>
2020-04-22 03:07:19 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,10 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA"
    return "I LOVE YOU TOO PUMPKIN!" 
 elsif phrase == phrase.upcase
-return ""
+return "HUH?! SPEAK UP, SONNY!."
+else
+  return
+  
 
 
 
